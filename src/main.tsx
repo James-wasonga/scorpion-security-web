b90d8e00bbcdd28a1cfdde12bbf600916505e06d
James-wasonga <jameswasonga22@gmail.com>
2025-08-01 23:57:06 +0300

    implement linking

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,3 @@
-// import { createRoot } from 'react-dom/client'
-// import App from './App.tsx'
-// import './index.css'
-
-// createRoot(document.getElementById("root")!).render(<App />);
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
